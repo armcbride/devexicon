d69f7ab761323942e1ebc0356be86b7f8df9ad76
peacefrog <gregroyclark@gmail.com>
2020-05-04 12:37:23 -0500

    sets up basic server file with express & PORT, leaves comments for development

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// brings in Handlebars, should we decide to use it
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// brings routes from controller.js
+// var routes = require("./controllers/awesomeController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
