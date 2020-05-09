901639b765d02942ba5a86fd280821e396b60861
Annaruth McBride <armcbride17@gmail.com>
2020-05-09 12:36:05 -0500

    create api routes for get all and get category

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,25 @@
 var db = require("../models");
 
 module.exports = function(app){
-  //CRUD  
-};
+  //GET route for all definitions
+  app.get("/api/definitions/", function(req, res) {
+    db.Definitions.findAll({})
+      .then(function(dbDefinitions) {
+        res.json(dbDefinitions);
+      });
+  });
+
+//GET route for all in specific category
+  app.get("/api/definitions/category/:category", function(req, res) {
+    db.Definitions.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function(dbDefinitions) {
+        res.json(dbDefinitions);
+      });
+  });
+
+
+}
