09b888ac73201f67d575be9238641db783aa9835
Annaruth McBride <armcbride17@gmail.com>
2020-05-14 19:50:30 -0500

    created seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,17 @@
+
+USE sdz9w1lbrjjeliul;
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Javascript', 'A programming language used to make a site more interactable.', '', 'Javascript');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Variables','Stores data values that can be used to add dynanicism to the application. Variables should have unique names that define the data that it holds.','var score = 0','Javascript');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Functions','A set of statements that performs tasks or calculates a value','function numbers(num){ return num* num}','JavaScript');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Objects','A collection of named values.','car = {make:'Hyundai', model:'Sonata', color:'orange', year:2014}','JavaScript');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Properties', 'The values connected to an object.', 'car.model','javaScript')
