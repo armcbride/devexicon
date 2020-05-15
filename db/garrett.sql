e72cdab86e22adc9683c255a5b6318a6fb39184f
garrett griffey <garrettwgriffey@gmail.com>
2020-05-14 20:53:21 -0500

    added html facts to garrett.md

---
+++ db/garrett.sql
@@ -0,0 +1,30 @@
+USE sdz9w1lbrjjeliul;
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('HTML', 'Hyper-Text Markup Language is the standard markup language for creating web pages.', '', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Tags','HTML tags label pieces of content such as "heading", "paragraph", "table", and so on.','<h1>Hello World!(/h1>','HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Elements','An HTML element is defined by a start tag, some content, and an end tag.','<tagname>Content goes here...</tagname>','HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Attributes','An attribute provides additional information about an HTML element and usually come in name/value pairs like: name="value".','HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Links', 'HTML links are known as Hyperlinks and direct the user to other web addresses or pages within the same URL.', '<a href="url">link text</a>','HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('HTML', 'Hyper Text Markup Language is for creating a webpage. HTML includes writing, links, pictures, sound, and even video', '<!DOCTYPE html>
+<html lang="en">
+  <head></head><body></body></html>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Tags', 'Hidden keywords within a web page that define how the web browser formats and displays the content. Most tags must have two parts, an opening and a closing part.', '<body> </body>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Element', 'the content of a start and end tag and everything in between', '<body> Hello World </body>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Attributes', 'Provide additional information about an element and are specified in the start tag', '<img src= "happy_cat.gif">', 'HTML');
