00ac8664413925d0e6df515a3b598671efa455ce
Annaruth McBride <armcbride17@gmail.com>
2020-05-14 19:22:31 -0500

    stash

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,10 +10,10 @@ module.exports = function(app){
   });
 
 //GET route for all in specific category
-  app.get("/api/definitions/category/:category", function(req, res) {
+  app.get("/api/definitions/language/:language", function(req, res) {
     db.Definitions.findAll({
       where: {
-        category: req.params.category
+        language: req.params.language
       }
     })
       .then(function(dbDefinitions) {
@@ -28,7 +28,7 @@ module.exports = function(app){
       title: req.body.title,
       definition: req.body.definition,
       example: req.body.example,
-      category: req.body.category
+      language: req.body.language
     })
       .then(function(dbDefinitions) {
         res.json(dbDefinitions);
