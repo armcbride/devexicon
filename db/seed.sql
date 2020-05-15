ebc4fd339fdb8d212a21eed221658e0e759b70ef
Annaruth McBride <armcbride17@gmail.com>
2020-05-14 19:59:29 -0500

    added more to seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -14,4 +14,18 @@ INSERT INTO Definitions (topic, definition, example, language)
 VALUES ('Objects','A collection of named values.','car = {make:'Hyundai', model:'Sonata', color:'orange', year:2014}','JavaScript');
 
 INSERT INTO Definitions (topic, definition, example, language)
-VALUES ('Properties', 'The values connected to an object.', 'car.model','javaScript')
+VALUES ('Properties', 'The values connected to an object.', 'car.model','javaScript');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('HTML', 'Hyper Text Markup Language is for creating a webpage. HTML includes writing, links, pictures, sound, and even video', '<!DOCTYPE html>
+<html lang="en">
+  <head></head><body></body></html>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Tags', 'Hidden keywords within a web page that define how the web browser formats and displays the content. Most tags must have two parts, an opening and a closing part.', '<body> </body>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Element', 'the content of a start and end tag and everything in between', '<body> Hello World </body>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Attributes', 'Provide additional information about an element and are specified in the start tag', '<img src= "happy_cat.gif">', 'HTML');
