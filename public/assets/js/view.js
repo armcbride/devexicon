509c3847464665b20b7c411c6287bee2490d4bc1
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-05-16 10:59:45 -0500

    appended result to the page

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+  let definitions = document.getElementById('table');
+
   function getDefinitions() {
       $.ajax({
         method: "GET",
         url: "/api/definitions/",
       }).then((res) => {
         console.log(res);
+        let newDefinition = JSON.stringify(res);
+        definitions.append(newDefinition);
       });
     };
     getDefinitions();
