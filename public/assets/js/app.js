a1c57ed4112eadb183e649710e2557a13b8fbd9d
Annaruth McBride <armcbride17@gmail.com>
2020-05-16 09:27:36 -0500

    create new definition route works

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
+
     function getDefinitions() {
         $.get('/api/definitions/', function(data, status) {
             console.log('Data: ' + data + '\nStatus: ' + status);
         });
     };
     getDefinitions();
+    
+    $('#add-btn').on('click', function(event){
+        event.preventDefault();
+
+        let newDefinition = {
+            topic: $('#topic').val().trim(),
+            definition: $('#definition').val().trim(),
+            example: $('#example').val().trim(),
+            language: $('#language').val().trim()
+        };
+        $.post("api/definition", newDefinition).then(function(response){
+            console.log(response);
+        });
+
+        $('#topic').val('');
+        $('#definition').val('');
+        $('#example').val('');
+        $('#language').val('');
+    });
 });
