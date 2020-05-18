ed21df0920a14524116aa43e06270c33235161f0
Annaruth McBride <armcbride17@gmail.com>
2020-05-18 14:58:06 -0500

    adding CSS definitions to seed, added notes to organize definitions

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,7 @@
 
 USE sdz9w1lbrjjeliul;
 
+-- Javascript
 INSERT INTO Definitions (topic, definition, example, language)
 VALUES ('Javascript', 'A programming language used to make a site more interactable.', '', 'Javascript');
 
@@ -16,6 +17,7 @@ VALUES ('Objects','A collection of named values.','car = {make:"Hyundai", model:
 INSERT INTO Definitions (topic, definition, example, language)
 VALUES ('Properties', 'The values connected to an object.', 'car.model','javaScript');
 
+--HTML
 INSERT INTO Definitions (topic, definition, example, language)
 VALUES ('HTML', 'Hyper Text Markup Language is for creating a webpage. HTML includes writing, links, pictures, sound, and even video', '<!DOCTYPE html>
 <html lang="en">
@@ -28,4 +30,26 @@ INSERT INTO Definitions (topic, definition, example, language)
 VALUES ('Element', 'the content of a start and end tag and everything in between', '<body> Hello World </body>', 'HTML');
 
 INSERT INTO Definitions (topic, definition, example, language)
-VALUES ('Attributes', 'Provide additional information about an element and are specified in the start tag', '<img src= "happy_cat.gif">', 'HTML');
+VALUES ('Attributes', 'Provide additional information about an element and are specified in the start tag', '<img src= "happy_cat.gif">, <a href="https://cats.com</a>', 'HTML');
+
+INSERT INTO Definitions (topic, definition, example, language)
+VALUES ('Links', 'HTML links are known as Hyperlinks and direct the user to other web addresses or pages within the same URL.', '<a href="url">link text</a>','HTML');
+
+--CSS
+INSERT INTO Definitions(topic, definition, example, language)
+VALUES ('CSS','cascading style sheet which describes how HTML elements are displayed on screen','body {background-color: white;}','CSS');
+
+INSERT INTO Definitions(topic, definition, example, language)
+VALUES ('CSS Syntax','consists of a selector and a delcaration block','Selector = h1, declaration = color:blue; font-size: 16px; alltogether = h1{color:blue; font-size: 16px;}','CSS');
+
+INSERT INTO Definitions(topic, definition, example, language)
+VALUES ('Styles','effect design, layout, variations, animation and screen dimensions','{max-width: 900px; border-shadow: 6px;}','CSS');
+
+INSERT INTO Definitions(topic, definition, example, language)
+VALUES ('Property','the specific style being altered for a selector','h1{font-size: 14px;}, here font-size is the property','CSS');
+
+INSERT INTO Definitions(topic, definition, example, language)
+VALUES ('Property value','what the specific style being altered is going to be','h1{font-size: 14px;}, 14px is the property value','CSS');
+
+INSERT INTO Definitions(topic, definition, example, language)
+VALUES ('Selectors','There are several categories of different selectors used to find the HTML elements to style: simple, combinator, pseudo-class, pseudo-elements selectors, and attribute selectors','simple: elements based on name, id, and class; combinator: elements based on a specific relationship between them; pseudo-class: elements based on a certain state; pseudo-elements: select and style part of an element; attribute: elements based on an attribute or attribute value','CSS');
