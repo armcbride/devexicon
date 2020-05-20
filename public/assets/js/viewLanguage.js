4220b483efda0a7285bd0aaa928f197cb0f70a7d
Annaruth McBride <armcbride17@gmail.com>
2020-05-19 20:57:55 -0500

    template literal in viewLanguage.js, and cleaned code in html

--- public/assets/js/viewLanguage.js
+++ public/assets/js/viewLanguage.js
@@ -9,14 +9,11 @@ $(document).ready(function () {
         $.get('/api/definitions/language/' + languageSearch, function(res) {
             console.log(res);
             res.forEach(language => {
-                table.append('<p>' + language.id + '</p>');
-                table.append('<p>Topic: ' + language.topic + '</p>');
-                table.append('<p>Definition: ' + language.definition + '</p>');
-                table.append('<p>Example:<textarea> ' + language.example + '</textarea></p>');
-                table.append('<p>Language: ' + language.language + '</p>');
-                table.append('<p>Created At: ' + language.createdAt + '</p>');
-                table.append('<p>Updated At: ' + language.updatedAt + '</p>');
-            });
+                table.append(`<p>${language.id}</p>
+                                <p>Topic: ${language.topic}</p>
+                                <p>Definition: ${language.definition}</p>
+                                <p>Example: <textarea>${language.example}</textarea></p>
+                                <p>Language: ${language.language}</p>`);
         });
     });
 });
