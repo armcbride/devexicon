f05bcf766577b49d785d56a6a0adbd27d80eb33c
garrett griffey <garrettwgriffey@gmail.com>
2020-05-20 22:44:17 -0500

    added 2nd option with uikit cards instead of bootstrap

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -18,15 +18,23 @@ $(document).ready(function () {
         //   <p>Language: ${definition.language}</p></div>
          
 
-$deck.append(` <div class="card" style="width: 18rem; box-shadow: 5px 10px; margin: 5px;">
+$deck.append
+// (` <div class="card" style="width: 18rem; box-shadow: 5px 10px; margin: 5px;">
 
-<div class="card-body">
-  <h3 class="card-title">${definition.language}</h3>
-  <p class="card-text">Topic: ${definition.topic}</p>
-  <p class="card-text">Definition: ${definition.definition}</p>
-  <p class="card-text">Example: <textarea readonly>${definition.example}</textarea></p>
-</div>
-</div>`)
+// <div class="card-body">
+//   <h3 class="card-title">${definition.language}</h3>
+//   <p class="card-text">Topic: ${definition.topic}</p>
+//   <p class="card-text">Definition: ${definition.definition}</p>
+//   <p class="card-text">Example: <textarea readonly>${definition.example}</textarea></p>
+// </div>
+// </div>`)
+
+(`<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+    <div class="uk-card-badge uk-label">${definition.language}</div>
+    <h3 class="uk-card-title">Topic: ${definition.topic}</h3>
+    <p>Definition: ${definition.definition}</p>
+    <div>Example: <textarea readonly>${definition.example}</div>
+</div>`);
         });
       });
     }
