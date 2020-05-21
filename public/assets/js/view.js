f8d38b0e4c49fd27e23fabb491370aef312d3a8a
garrett griffey <garrettwgriffey@gmail.com>
2020-05-20 20:41:47 -0500

    cards added

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  let list = document.querySelector('.list-group');
-
+  // let list = document.querySelector('.list-group');
+  const $deck=$('.deck')
   function getDefinitions() {
       $.ajax({
         method: "GET",
@@ -8,14 +8,25 @@ $(document).ready(function () {
       }).then((res) => {
 
         res.forEach(definition => {
-         let li = $('<li/>')
-          .appendTo(list);
+        //  let li = $('<li/>')
+        //   .appendTo(list);
 
-li.append(`<div class= "definitions">${definition.id}</p>
-          <p>Topic: ${definition.topic}</p>
-          <p>Definition: ${definition.definition}</p>
-          <p>Example: <textarea readonly>${definition.example}</textarea></p>
-          <p>Language: ${definition.language}</p></div>`)
+        // <div class= "definitions">${definition.id}</p>
+        //   <p>Topic: ${definition.topic}</p>
+        //   <p>Definition: ${definition.definition}</p>
+        //   <p>Example: <textarea readonly>${definition.example}</textarea></p>
+        //   <p>Language: ${definition.language}</p></div>
+         
+
+$deck.append(` <div class="card" style="width: 18rem;">
+
+<div class="card-body">
+  <h5 class="card-title">Topic: ${definition.language}</h5>
+  <p class="card-text">Example: <textarea readonly>${definition.example}</textarea></p>
+  <p class="card-text">Definition: ${definition.definition}</p>
+  <p class="card-text">Topic: ${definition.topic}</p>
+</div>
+</div>`)
         });
       });
     };
