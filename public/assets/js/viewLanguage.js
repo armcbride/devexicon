3f2cfc6d5c6b6400830467010f6f013fa0c5adc6
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-05-21 17:00:08 -0500

    added onclick events for buttons

--- public/assets/js/viewLanguage.js
+++ public/assets/js/viewLanguage.js
@@ -1,15 +1,52 @@
-let table = $('#table');
+let htmlData = $('#htmlData');
+let cssData = $('#cssData');
+let javascriptData = $('#javascriptData');
 
 $(document).ready(function () {
-    let table = $('#table');
 
-    $('.language').on('click', function() {
+    $('#html').on('click', function() {
+        htmlData.empty();
+
         let languageSearch = $(this).html();
 
         $.get('/api/definitions/language/' + languageSearch, function(res) {
             console.log(res);
             res.forEach(language => {
-                table.append(`<p>${language.id}</p>
+                htmlData.append(`<p>${language.id}</p>
+                <p>Topic: ${language.topic}</p>
+                <p>Definition: ${language.definition}</p>
+                <p>Example: <textarea readonly>${language.example}</textarea></p>
+                <p>Language: ${language.language}</p>`);
+            });
+        });
+    });
+
+    $('#css').on('click', function() {
+        cssData.empty();
+
+        let languageSearch = $(this).html();
+
+        $.get('/api/definitions/language/' + languageSearch, function(res) {
+            console.log(res);
+            res.forEach(language => {
+                cssData.append(`<p>${language.id}</p>
+                <p>Topic: ${language.topic}</p>
+                <p>Definition: ${language.definition}</p>
+                <p>Example: <textarea readonly>${language.example}</textarea></p>
+                <p>Language: ${language.language}</p>`);
+            });
+        });
+    });
+
+    $('#javascript').on('click', function() {
+        javascriptData.empty();
+        
+        let languageSearch = $(this).html();
+
+        $.get('/api/definitions/language/' + languageSearch, function(res) {
+            console.log(res);
+            res.forEach(language => {
+                javascriptData.append(`<p>${language.id}</p>
                 <p>Topic: ${language.topic}</p>
                 <p>Definition: ${language.definition}</p>
                 <p>Example: <textarea readonly>${language.example}</textarea></p>
